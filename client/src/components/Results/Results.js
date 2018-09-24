71fe6cf99817040323e96c53b533b9d822171886
lindnerdesign <lindnerdesign@gmail.com>
2018-09-24 18:19:42 -0500

    updates

--- client/src/components/Results/Results.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
-
-const Results = () => ( 
-  <h1 className="results">Article Results</h1>
-);
-
-export default Results;
--- client/src/components/Saved/Saved.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
-
-const Saved = () => ( 
-  <h1 className="results">Saved Articles</h1>
-);
-
-export default Saved;
